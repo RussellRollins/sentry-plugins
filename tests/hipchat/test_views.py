c75dd1a35f4b1af738def50025f98df5c02d2ea7
Russell Rollins <russell.rollins@salesforce.com>
2017-05-01 18:54:11 -0400

    Change test to follow variable naming style.

--- tests/hipchat/test_views.py
+++ tests/hipchat/test_views.py
@@ -8,8 +8,8 @@ from sentry_plugins.hipchat_ac import views
 class HipchatPluginTest(PluginTestCase):
     def test_link_regex(self):
         regex = views.get_link_regexp()
-        matchOld = regex.search(views.options.get('system.url-prefix') + "org/proj/group/123/events/456")
-        assert matchOld is not None
+        match_old = regex.search(views.options.get('system.url-prefix') + "org/proj/group/123/events/456")
+        assert match_old is not None
 
-        matchNew = regex.search(views.options.get('system.url-prefix') + "org/proj/issues/123/events/456")
-        assert matchNew is not None
+        match_new = regex.search(views.options.get('system.url-prefix') + "org/proj/issues/123/events/456")
+        assert match_new is not None
