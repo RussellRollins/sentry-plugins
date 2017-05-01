e091ac83bdcbca6df62c9cab2d18de6bad5f8d2c
Russell Rollins <russell.rollins@salesforce.com>
2017-05-01 19:20:51 -0400

    More test simplification.

--- tests/hipchat/test_views.py
+++ tests/hipchat/test_views.py
@@ -7,9 +7,8 @@ from sentry_plugins.hipchat_ac import views
 
 class HipchatPluginTest(PluginTestCase):
     def test_link_regex(self):
-        regex = views.get_link_regexp()
-        match_old = regex.search(views.options.get('system.url-prefix') + "org/proj/group/123/events/456")
+        match_old = views.get_link_regexp().search(views.options.get('system.url-prefix') + "org/proj/group/123/events/456")
         assert match_old is not None
 
-        match_new = regex.search(views.options.get('system.url-prefix') + "org/proj/issues/123/events/456")
+        match_new = views.get_link_regexp().search(views.options.get('system.url-prefix') + "org/proj/issues/123/events/456")
         assert match_new is not None
